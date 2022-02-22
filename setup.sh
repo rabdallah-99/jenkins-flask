1d7a4537258452b348b6687d4f21273fd857c51a
rabdallah-99 <74275970+rabdallah-99@users.noreply.github.com>
2022-02-22 16:13:21 +0000

    Update setup.sh
    RA: update setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
+sudo apt update
+sudo apt install python3 python3-venv python3-pip -y
+if (-d jenkins-flask ) 
+  then 
+         git clone https://github.com/rabdallah-99/jenkins-flask
+fi
 cd jenkins-flask
 python3 -m venv
 . ./venv/bin/activate
